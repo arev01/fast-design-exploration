7796c23aece24e61afa3aa74d6443553cf75d03e
Antoine Reverberi <106743110+arev01@users.noreply.github.com>
2023-07-14 12:59:43 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,40 +4,38 @@ import numpy as np
 
 st.title('Uber pickups in NYC')
 
-FILE_TYPES = ["csv", "py", "png", "jpg"]
-
-file = st.file_uploader("Upload file", type=FILE_TYPES)
-show_file = st.empty()
-if not file:
-    show_file.info("Please upload a file of type: " + ", ".join(FILE_TYPES))
-
-DATE_COLUMN = 'date/time'
-DATA_URL = ('https://s3-us-west-2.amazonaws.com/'
-            'streamlit-demo-data/uber-raw-data-sep14.csv.gz')
-
 @st.cache_data
-def load_data(nrows):
-    data = pd.read_csv(DATA_URL, nrows=nrows)
+def load_data(filename):
+    data = pd.read_csv(filename)
     lowercase = lambda x: str(x).lower()
     data.rename(lowercase, axis='columns', inplace=True)
-    data[DATE_COLUMN] = pd.to_datetime(data[DATE_COLUMN])
+    #data[DATE_COLUMN] = pd.to_datetime(data[DATE_COLUMN])
     return data
 
+FILE_TYPES = ["csv", "py", "png", "jpg"]
+
+file_in = st.file_uploader("Upload file", type=FILE_TYPES)
+show_file = st.empty()
+if not file_in:
+    show_file.info("Please upload a file of type: " + ", ".join(FILE_TYPES))
+    
 data_load_state = st.text('Loading data...')
-data = load_data(10000)
+data = load_data(file_in)
 data_load_state.text("Done! (using st.cache_data)")
+
+nb_params = len(data.columns)
 
 if st.checkbox('Show raw data'):
     st.subheader('Raw data')
     st.write(data)
 
-st.subheader('Number of pickups by hour')
-hist_values = np.histogram(data[DATE_COLUMN].dt.hour, bins=24, range=(0,24))[0]
-st.bar_chart(hist_values)
-
 # Some number in the range 0-23
-hour_to_filter = st.slider('hour', 0, 23, 17)
-filtered_data = data[data[DATE_COLUMN].dt.hour == hour_to_filter]
+for name in data.columns:
+    min = data[name].min()
+    max = data[name].max()
+    
+    filter = st.slider(name, min, max, min)
+#filtered_data = data[data[DATE_COLUMN].dt.hour == hour_to_filter]
 
-st.subheader('Map of all pickups at %s:00' % hour_to_filter)
-st.map(filtered_data)
+#st.subheader('Map of all pickups at %s:00' % hour_to_filter)
+#st.map(filtered_data)
