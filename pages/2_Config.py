22288a8fb4ac296a84710a6f34b1c34a7ec214b2
Antoine Reverberi <106743110+arev01@users.noreply.github.com>
2023-10-11 15:07:30 +0200

    Update 2_Config.py

--- pages/2_Config.py
+++ pages/2_Config.py
@@ -8,3 +8,21 @@ st.markdown(
     Configures the default settings of the page.
     """
 )
+
+separator = st.selectbox("Select the delimiter", options=[
+    ",", 
+    ";"
+    ], 
+    key="select_separator")
+
+if separator == ";":
+    st.session_state["separator"] = ";"
+    
+decimal = st.selectbox("Select the decimal separator", options=[
+    ".", 
+    ","
+    ], 
+    key="select_decimal")
+
+if decimal == ",":
+    st.session_state["decimal"] = ","
